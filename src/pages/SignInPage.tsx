0b5d057e97cf51b1f20386d6e1a7a890258e32c4
lwwywang <liwenwang4work@163.com>
2024-02-25 18:12:42 +0800

    处理登录成功和失败

--- src/pages/SignInPage.tsx
+++ src/pages/SignInPage.tsx
@@ -1,17 +1,23 @@
 import type { FormEventHandler } from 'react'
 import { useNavigate } from 'react-router-dom'
+import type { AxiosError } from 'axios';
+import axios from 'axios'
 import { Gradient } from '../components/Gradient'
 import { Icon } from '../components/Icon'
 import { TopNav } from '../components/TopNav'
 import { ajax } from '../lib/ajax'
+import type { FormError } from '../lib/validate';
 import { hasError, validate } from '../lib/validate'
 import { useSignInStore } from '../stores/useSignInStore'
 import { Input } from '../components/Input'
-import axios from 'axios'
 
 export const SignInPage: React.FC = () => {
   const { data, error, setData, setError } = useSignInStore()
   const nav = useNavigate()
+  const onSubmitError = (err: AxiosError<{ errors: FormError<typeof data> }>) => {
+    setError(err.response?.data?.errors ?? {})
+    throw error
+  }
   const onSubmit: FormEventHandler<HTMLFormElement> = async (e) => {
     e.preventDefault()
     const newError = validate(data, [
@@ -22,7 +28,11 @@ export const SignInPage: React.FC = () => {
     ])
     setError(newError)
     if (!hasError(newError)) {
-      await ajax.post('/api/v1/session', data)
+      const response = await ajax.post<{ jwt: string }>('http://121.196.236.94:8080/api/v1/session', data)
+        .catch(onSubmitError)
+      const jwt = response.data.jwt
+      console.log('jwt', jwt)
+      localStorage.setItem('jwt', jwt)
       // TODO
       // 保存 JWT 作为登录凭证
       nav('/home')
